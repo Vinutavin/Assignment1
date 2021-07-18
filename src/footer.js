edfac26c5cb9995fcfc097ba7802b9ca20cb6604
Vinutavin <34506657+Vinutavin@users.noreply.github.com>
2021-07-18 18:09:11 +0530

    Added footer content

--- src/footer.js
+++ src/footer.js
@@ -2,6 +2,8 @@ export default function Footer(props) {
   return (
     <div classname="footer">
       <h3>Footer 2021</h3>
+      <p>Footer content</p>
+      <p>Footer content</p>
     </div>
   );
 }
